f751d291e721d25669da6b558963c467ab1c1ea8
hirano-vm4 <hirano.vm4@gmail.com>
2023-10-16 14:30:44 +0900

    js版FizzBuzzの課題提出

---
+++ 01.fizzbuzz/my-fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(n) {
+  if (n < 1) {
+    console.log("1以上の整数を入力してください");
+    return;
+  }
+
+  for (let num = 1; num <= n; num++) {
+    let output = "";
+    if (num % 3 === 0) output += "Fizz";
+    if (num % 5 === 0) output += "Buzz";
+    console.log(output || num);
+  }
+}
+
+fizzBuzz(20);
