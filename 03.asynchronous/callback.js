181f22d199fbd7e8154d2e132fc3757f997ab3bc
hirano-vm4 <hirano.vm4@gmail.com>
2023-11-18 10:34:27 +0900

    【課題提出】コールバック形式での記述

---
+++ 03.asynchronous/callback.js
@@ -0,0 +1,43 @@
+// 1. コールバック（エラーなし）
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run(
+      "INSERT INTO books(title) VALUES('ゼロからわかるRuby超入門')",
+      () => {
+        db.get("SELECT id FROM books", (_err, row) => {
+          console.log(`id: ${row.id}`);
+          db.get("SELECT * FROM books", (_err, row) => {
+            console.log(row);
+            db.run("DROP TABLE books", () => {
+              console.log("テーブルを削除しました");
+            });
+          });
+        });
+      }
+    );
+  }
+);
+
+await timers.setTimeout(1000);
+
+// 1. コールバック（エラーあり）
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run("INSERT INTO books(title) VALUES(null)", (err) => {
+      if (err) console.log(`レコード追加エラー: ${err.message}`);
+      db.get("SELECT * FROM errs", (err) => {
+        if (err) console.log(`レコードの取得エラー: ${err.message}`);
+        db.run("DROP TABLE books", () => {
+          console.log("テーブルを削除しました");
+        });
+      });
+    });
+  }
+);
