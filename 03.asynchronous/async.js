78484349046f43a89674ad069b6f22bfafe1f8fd
hirano-vm4 <hirano.vm4@gmail.com>
2023-11-18 10:35:56 +0900

    【課題提出】async / awaitを使った非同期処理

---
+++ 03.asynchronous/async.js
@@ -0,0 +1,37 @@
+import timers from "timers/promises";
+import * as bookTableOperations from "./table_function.js";
+
+// 3. async / await （エラーなし）
+
+const noErr = async () => {
+  await bookTableOperations.createTable();
+  await bookTableOperations.insertTitle();
+  await bookTableOperations.getId();
+  await bookTableOperations.getRecord();
+  await bookTableOperations.deleteRecord();
+};
+
+noErr();
+
+await timers.setTimeout(1000);
+
+// 3. async / await （エラーあり）
+const err = async () => {
+  await bookTableOperations.createTable();
+
+  try {
+    await bookTableOperations.insertTitleErr();
+  } catch (err) {
+    console.log(`レコード追加エラー: ${err.message}`);
+  }
+
+  try {
+    await bookTableOperations.getRecordErr();
+  } catch (err) {
+    console.log(`レコードの取得エラー: ${err.message}`);
+  }
+
+  await bookTableOperations.deleteRecord();
+};
+
+err();
