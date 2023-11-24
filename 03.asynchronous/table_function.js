b98223f1e14cf0aba0784a28c2bb58c1d3776a8b
hirano-vm4 <hirano.vm4@gmail.com>
2023-11-24 17:23:35 +0900

    node-sqlite3の関数をPromiseベースにする関数になるよう修正

--- 03.asynchronous/table_function.js
+++ 03.asynchronous/table_function.js
@@ -1,65 +1,23 @@
-import sqlite3 from "sqlite3";
-
-const db = new sqlite3.Database(":memory:");
-
-export const createTable = () => {
+export const run = (db, sql) => {
   return new Promise((resolve, reject) => {
-    db.run(
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-      (err) => {
-        err ? reject(err) : resolve();
+    db.run(sql, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this);
       }
-    );
+    });
   });
 };
 
-export const insertTitle = () => {
+export const get = (db, sql) => {
   return new Promise((resolve, reject) => {
-    db.run(
-      "INSERT INTO books(title) VALUES('ゼロからわかるRuby超入門')",
-      (err) => {
-        err ? reject(err) : resolve();
+    db.get(sql, function (err, row) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
       }
-    );
-  });
-};
-
-export const insertTitleErr = () => {
-  return new Promise((resolve, reject) => {
-    db.run("INSERT INTO books(title) VALUES(null)", (err) => {
-      err ? reject(err) : resolve();
-    });
-  });
-};
-
-export const getId = () => {
-  return new Promise((resolve, reject) => {
-    db.get("SELECT id FROM books", (err, row) => {
-      err ? reject(err) : resolve(console.log(`id: ${row.id}`));
-    });
-  });
-};
-
-export const getRecord = () => {
-  return new Promise((resolve, reject) => {
-    db.get("SELECT * FROM books", (err, row) => {
-      err ? reject(err) : resolve(console.log(row));
-    });
-  });
-};
-
-export const getRecordErr = () => {
-  return new Promise((resolve, reject) => {
-    db.get("SELECT * FROM errs", (err) => {
-      err ? reject(err) : resolve();
-    });
-  });
-};
-
-export const deleteRecord = () => {
-  return new Promise((resolve, reject) => {
-    db.run("DROP TABLE books", (err) => {
-      err ? reject(err) : resolve(console.log("テーブルを削除しました"));
     });
   });
 };
